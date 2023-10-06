a1e49e6e92f2ff79b71fc2d6f75bbcb490f20a6e
LoraStoilova <office@amtek-bg.com>
2023-10-06 13:04:53 +0300

    First version

---
+++ ListBasicsExercise/invert_values1.py
@@ -0,0 +1,8 @@
+string_numbers = input().split()
+opposite_numbers = []
+
+for number in string_numbers:
+    current_number = -int(number)
+    opposite_numbers.append(current_number)
+
+print(opposite_numbers)
